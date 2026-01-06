75a968982233747ed9dcbc731156d01208838617
Leas Porscha <leas@porscha.eu>
2026-01-06 02:06:57 +0100

    fix(examples): import

--- examples/lib_example.py
+++ examples/lib_example.py
@@ -2,7 +2,7 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from displaypad import DisplayPad, Icon
+from displaypad_lib import DisplayPad, Icon
 import logging
 import random
 
